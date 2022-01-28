e478574804bdca6b86ba01fd797c8d5c875b35f6
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2022-01-28 15:29:38 +0300

    no message

---
+++ Package.swift
@@ -0,0 +1,9 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "leveldb",
+    products: [.library(name: "leveldb", targets: ["leveldb"])],
+    targets: [.systemLibrary(name: "leveldb")]
+)
