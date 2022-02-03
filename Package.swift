bba81044f806b41e43ca87f9590c3594b2cf656f
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2022-02-03 14:43:22 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "leveldb",
-    products: [.library(name: "leveldb", targets: ["leveldb"])],
-    targets: [.systemLibrary(name: "leveldb", pkgConfig: "leveldb")]
+    products: [.library(name: "leveldb", targets: ["package"])],
+    targets: [.systemLibrary(name: "leveldb", pkgConfig: "leveldb"), .target(name: "package", dependencies: ["leveldb"])]
 )
