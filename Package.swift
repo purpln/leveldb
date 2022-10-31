2262fb23ec5f03f3933a0babc1d9bf7afa041e77
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2022-10-31 15:41:05 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -1,9 +1,62 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 
 import PackageDescription
 
 let package = Package(
-    name: "leveldb",
+    name: "leveldb", platforms: [.iOS(.v11), .macOS(.v10_15)],
     products: [.library(name: "leveldb", targets: ["LevelDB"])],
-    targets: [.systemLibrary(name: "cleveldb", pkgConfig: "leveldb"), .target(name: "LevelDB", dependencies: ["cleveldb"])]
+    targets: [
+        .target(name: "LevelDB", dependencies: ["cleveldb"]),
+        .target(name: "cleveldb", exclude: [
+            "db/leveldbutil.cc",
+            "util/env_windows.cc",
+            "util/testutil.cc",
+            "db/autocompact_test.cc",
+            "db/corruption_test.cc",
+            "db/db_test.cc",
+            "db/dbformat_test.cc",
+            "db/fault_injection_test.cc",
+            "db/filename_test.cc",
+            "db/log_test.cc",
+            "db/recovery_test.cc",
+            "db/skiplist_test.cc",
+            "db/version_edit_test.cc",
+            "db/version_set_test.cc",
+            "db/write_batch_test.cc",
+            "issues/issue178_test.cc",
+            "issues/issue200_test.cc",
+            "issues/issue320_test.cc",
+            "table/filter_block_test.cc",
+            "table/table_test.cc",
+            "util/arena_test.cc",
+            "util/bloom_test.cc",
+            "util/cache_test.cc",
+            "util/coding_test.cc",
+            "util/crc32c_test.cc",
+            "util/env_posix_test.cc",
+            "util/env_test.cc",
+            "util/env_windows_test.cc",
+            "util/hash_test.cc",
+            "util/logging_test.cc",
+            "util/no_destructor_test.cc",
+            "util/status_test.cc",
+            "db/c_test.c",
+            "port/README.md",
+            "port/port_config.h.in"
+        ], sources: [
+            "db/",
+            "port/",
+            "table/",
+            "util/",
+            "include/",
+        ], publicHeadersPath: "include", cSettings: [
+            .define("LEVELDB_IS_BIG_ENDIAN", to: "0"),
+            .define("LEVELDB_PLATFORM_POSIX", to: "1"),
+            .define("HAVE_FULLFSYNC", to: "1"),
+            .headerSearchPath("./"),
+            .headerSearchPath("include/")
+        ]),
+    ],
+    cLanguageStandard: .c11,
+    cxxLanguageStandard: .cxx11
 )
--- Sources/cleveldb/leveldb.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-#include "../../leveldb/include/leveldb/c.h"
